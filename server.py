7c8e2925c82551f257a3e277950c798aa2bb2fbd
tobyshooters <cristobal.sciutto@gmail.com>
2022-08-12 05:56:12 -0400

    listing of user's images under /user

--- server.py
+++ server.py
@@ -1,24 +1,30 @@
-from PIL import Image
 from flask import (
     Flask,
     render_template,
     request
 )
+import glob
+from PIL import Image
 
 app = Flask(__name__, template_folder=".")
 
 
 @app.route("/")
+def index():
+    return "hyperpixel"
+
 @app.route("/<user>")
-@app.route("/<user>/<image_id>")
-def user(user=None, image_id=None):
-    return render_template("index.html", user=user, imageId=image_id)
+def user(user):
+    paths = glob.glob(f"./static/{user}_*.jpg")
+    return render_template("listing.html", user=user, paths=paths)
 
-
-@app.route("/<user>/<image_id>", methods=['POST'])
+@app.route("/<user>/<image_id>", methods=['GET', 'POST'])
 def user_image(user, image_id):
-    image = request.files.get('file', '')
-    image = Image.open(image)
-    image.save(f"static/{user}_{image_id}.jpg")
-    return f"success", 200
+    if request.method == 'POST':
+        image = request.files.get('file', '')
+        image = Image.open(image)
+        image.save(f"./static/{user}_{image_id}.jpg")
+        return "success", 200
+    else:
+        return render_template("edit.html", user=user, imageId=image_id)
 
