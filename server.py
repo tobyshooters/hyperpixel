8e878045b6ed75831c3d311a387440acd67bba8b
tobyshooters <cristobal.sciutto@gmail.com>
2022-08-14 15:59:33 -0400

    store annotations in backend, Annotation has class for init + creation, for now show explicitly

--- server.py
+++ server.py
@@ -5,9 +5,9 @@ from flask import (
 )
 import glob
 from PIL import Image
+from collections import defaultdict
 
 app = Flask(__name__, template_folder=".")
-
 
 @app.route("/")
 def index():
@@ -26,4 +26,16 @@ def user_image(user, image_id):
         image.save(f"./static/{user}_{image_id}.jpg")
         return "success", 200
     else:
-        return render_template("edit.html", user=user, imageId=image_id)
+        annotations = imageAnnotations[(user, image_id)]
+        return render_template("edit.html", user=user, imageId=image_id, annotations=annotations)
+
+# Kept in memory for now
+imageAnnotations = defaultdict(dict)
+
+@app.route("/<user>/<image_id>/<annotation_id>", methods=['POST'])
+def user_image_annotation(user, image_id, annotation_id):
+    if request.json:
+        imageAnnotations[(user, image_id)][annotation_id] = request.json
+    else:
+        del imageAnnotations[(user, image_id)][annotation_id]
+    return "success", 200
