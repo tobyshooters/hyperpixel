624c77f0890a1568abf8c783f1b7b7cf49d436f4
tobyshooters <cristobal.sciutto@gmail.com>
2022-08-18 11:47:43 -0400

    backlinks!

--- server.py
+++ server.py
@@ -17,6 +17,12 @@ if os.path.exists("./static/annotations.pkl"):
 else:
     imageAnnotations = defaultdict(dict)
 
+if os.path.exists("./static/backlinks.pkl"):
+    with open('./static/backlinks.pkl', 'rb') as f:
+        imageBacklinks = pickle.load(f)
+else:
+    imageBacklinks = defaultdict(list)
+
 
 @app.route("/")
 def index():
@@ -38,11 +44,15 @@ def user_image(user, image_id):
         return "success", 200
     else:
         annotations = imageAnnotations[(user, image_id)]
+        print(annotations)
+        backlinks = imageBacklinks[(user, image_id)]
+        print(backlinks)
         return render_template(
             "edit.html",
             user=user,
             imageId=image_id,
-            annotations=annotations
+            annotations=annotations,
+            backlinks=backlinks,
         )
 
 
@@ -50,12 +60,25 @@ def user_image(user, image_id):
 def user_image_annotation(user, image_id, annotation_id):
     if request.json:
         imageAnnotations[(user, image_id)][annotation_id] = request.json
+
+        # Internal link! Track the backlinks
+        if request.json["href"][0] == "/":
+            [_, _user, _image_id] = request.json["href"].split("/")
+            if os.path.exists(f"./static/{_user}_{_image_id}.jpg"):
+                imageBacklinks[(_user, _image_id)].append((user, image_id))
+
     else:
         if annotation_id in imageAnnotations[(user, image_id)]:
             del imageAnnotations[(user, image_id)][annotation_id]
+            for k, v in imageBacklinks.items():
+                imageBacklinks[k] = [x for x in v if x != (user, image_id)]
+
 
     with open('./static/annotations.pkl', 'wb') as f:
         pickle.dump(imageAnnotations, f)
+
+    with open('./static/backlinks.pkl', 'wb') as f:
+        pickle.dump(imageBacklinks, f)
 
     return "success", 200
 
