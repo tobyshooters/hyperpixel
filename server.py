7f9bdf6fb18cf707e703dd61ec71a6f5cfaab549
tobyshooters <cristobal.sciutto@gmail.com>
2022-08-27 06:17:29 -0400

    unified db

--- server.py
+++ server.py
@@ -7,21 +7,33 @@ import os
 import glob
 import pickle
 from PIL import Image
-from collections import defaultdict
 
 app = Flask(__name__, template_folder=".")
 
-if os.path.exists("./static/annotations.pkl"):
-    with open('./static/annotations.pkl', 'rb') as f:
-        imageAnnotations = pickle.load(f)
-else:
-    imageAnnotations = defaultdict(dict)
+"""
+db = {
+    id: {
+        path: string
+        annotations; {
+            id: {
+                type: "internal" | "external",
+                id || href
+                x: float[0, 1],
+                x: float[0, 1],
+                w: float[0, 1],
+                h: float[0, 1],
+            }
+        }
+        backlinks: [ id: string ]
+    }
+}
 
-if os.path.exists("./static/backlinks.pkl"):
-    with open('./static/backlinks.pkl', 'rb') as f:
-        imageBacklinks = pickle.load(f)
+"""
+if os.path.exists("./static/db.pkl"):
+    with open('./static/db.pkl', 'rb') as f:
+        db = pickle.load(f)
 else:
-    imageBacklinks = defaultdict(list)
+    db = {}
 
 
 @app.route("/")
@@ -36,11 +48,24 @@ def image(image_id):
     if request.method == 'POST':
         image = request.files.get('file', '')
         image = Image.open(image)
-        image.save(f"./static/{image_id}.jpg")
+        path = f"./static/{image_id}.jpg"
+        image.save(path)
+
+        db[image_id] = {
+            "path": path,
+            "annotations": {},
+            "backlinks": []
+        }
+
         return "success", 200
     else:
-        annotations = imageAnnotations[image_id]
-        backlinks = imageBacklinks[image_id]
+        if image_id in db:
+            annotations = db[image_id]["annotations"]
+            backlinks = db[image_id]["backlinks"]
+        else:
+            annotations = []
+            backlinks = []
+
         return render_template(
             "edit.html",
             imageId=image_id,
@@ -52,26 +77,27 @@ def image(image_id):
 @app.route("/<image_id>/<annotation_id>", methods=['POST'])
 def image_annotation(image_id, annotation_id):
     if request.json:
-        imageAnnotations[image_id][annotation_id] = request.json
+        db[image_id]["annotations"][annotation_id] = request.json
 
         # Internal link! Track the backlinks
         if request.json["href"][0] == "/":
             [_, _image_id] = request.json["href"].split("/")
-            if os.path.exists(f"./static/{_image_id}.jpg"):
-                imageBacklinks[_image_id].append(image_id)
+
+            if _image_id in db:
+                db[_image_id]["backlinks"].append(image_id)
 
     else:
-        if annotation_id in imageAnnotations[image_id]:
-            del imageAnnotations[image_id][annotation_id]
-            for k, v in imageBacklinks.items():
-                imageBacklinks[k] = [x for x in v if x != image_id]
+        if annotation_id in db[image_id]["annotations"]:
+            del db[image_id]["annotations"][annotation_id]
+            for entry in db.values():
+                entry["backlinks"] = [
+                    other_id
+                    for other_id in entry["backlinks"]
+                    if other_id != image_id
+                ]
 
-
-    with open('./static/annotations.pkl', 'wb') as f:
-        pickle.dump(imageAnnotations, f)
-
-    with open('./static/backlinks.pkl', 'wb') as f:
-        pickle.dump(imageBacklinks, f)
+    with open('./static/db.pkl', 'wb') as f:
+        pickle.dump(db, f)
 
     return "success", 200
 
